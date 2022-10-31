04be397c2af8fdf015c542ec5a71ef4a55fce448
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2022-10-31 14:06:45 +0600

    completed banner section

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import img1 from '../../assets/images/banner/1.jpg'
+import img2 from '../../assets/images/banner/2.jpg'
+import img3 from '../../assets/images/banner/3.jpg'
+import img4 from '../../assets/images/banner/4.jpg'
+import img5 from '../../assets/images/banner/5.jpg'
+import img6 from '../../assets/images/banner/6.jpg'
+import BannerDetails from './BannerDetails'
+import './Home.css'
+const carouselData = [
+    {
+        img: img1,
+        id: 1,
+        prev: 6,
+        next: 2
+    },
+    {
+        img: img2,
+        id: 2,
+        prev: 1,
+        next: 3
+    },
+    {
+        img: img3,
+        id: 3,
+        prev: 2,
+        next: 4
+    },
+    {
+        img: img4,
+        id: 4,
+        prev: 3,
+        next: 5
+    },
+    {
+        img: img5,
+        id: 5,
+        prev: 4,
+        next: 6
+    },
+    {
+        img: img6,
+        id: 6,
+        prev: 5,
+        next: 1
+    }
+]
+
+const Banner = () => {
+  return (
+    <div className="carousel w-full mb-[130px]">
+        {
+            carouselData.map(data => <BannerDetails key={data.id} data={data}></BannerDetails>)
+        }
+</div>
+  )
+}
+
+export default Banner
---
+++ src/Pages/Home/BannerDetails.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const BannerDetails = ({data}) => {
+    const {img,id,prev,next} = data;
+  return (
+    <div id={`slide${id}`} className="carousel-item relative w-full ">
+    <div className='banner-img'>
+        <img src={img} className="w-full h-full rounded-md" />
+    </div>
+    <div className="absolute flex justify-end gap-2 transform -translate-y-1/2 right-5 bottom-5">
+      <a href={`#slide${prev}`} className="btn btn-circle">❮</a> 
+      <a href={`#slide${next}`} className="btn btn-circle">❯</a>
+    </div>
+    <div className="absolute text-white transform -translate-y-1/2 left-24 top-1/2 text-left ">
+      <h1 className='text-6xl font-bold text-white'>Affordable <br />
+       Price For Car <br />
+        Servicing</h1>
+        <p className='text-lg w-1/2 my-8'>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+        <button className='px-3 py-3 bg-orange-600 text-white rounded-md'>Discover More</button>
+        <button className='px-3 py-2 border-2 ml-5 text-white rounded-md'>Latest Project</button>
+    </div>
+  </div> 
+  )
+}
+
+export default BannerDetails
